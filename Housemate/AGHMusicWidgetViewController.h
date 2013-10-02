fd5fb8c0fcf76b750b3d80b60331ca6a61a7895c
achmizs <obormot@Arcturus.local>
2013-10-02 16:51:30 -0400

    New graphics assets.

---
+++ Housemate/AGHMusicWidgetViewController.h
@@ -0,0 +1,13 @@
+//
+//  AGHMusicWidgetViewController.h
+//  Housemate
+//
+//  Created by Sandy Achmiz on 10/2/13.
+//  Copyright (c) 2013 AGH. All rights reserved.
+//
+
+#import "AGHWidgetViewController.h"
+
+@interface AGHMusicWidgetViewController : AGHWidgetViewController
+
+@end
---
+++ Housemate/AGHMusicWidgetViewController.m
@@ -0,0 +1,27 @@
+//
+//  AGHMusicWidgetViewController.m
+//  Housemate
+//
+//  Created by Sandy Achmiz on 10/2/13.
+//  Copyright (c) 2013 AGH. All rights reserved.
+//
+
+#import "AGHMusicWidgetViewController.h"
+
+@interface AGHMusicWidgetViewController ()
+
+@end
+
+@implementation AGHMusicWidgetViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
